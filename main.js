9988dd46e2927abc25523839c2b8d3d11f42a893
Yumna <yumnamubeen0019@gmail.com>
2024-12-18 16:57:17 +0500

    home page

---
+++ main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed("#dynamic-name", {
+        strings: ["Yumna Mubeen", "a Frontend Developer", "a Creative Coder"],
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 1500,
+        startDelay: 500,
+        loop: true,
+        showCursor: true,
+        cursorChar: "|",
+    });
+});
