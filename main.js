2955bc22b236853449bb05ff85493fee88459ce5
Yumna <yumnamubeen0019@gmail.com>
2024-12-19 16:59:40 +0500

    about done

--- main.js
+++ main.js
@@ -10,3 +10,19 @@ document.addEventListener("DOMContentLoaded", function () {
         cursorChar: "|",
     });
 });
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        // tabcontent.classList.remove("active-tab");
+        tabcontent.style.display = "none";
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).style.display = "block";
+    // document.getElementById(tabname).classList.add("active-tab");
+}
