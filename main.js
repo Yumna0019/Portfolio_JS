926a73d6e34f9913193659697577a10ed815cdc1
Yumna <yumnamubeen0019@gmail.com>
2024-12-21 16:27:32 +0500

    contac sheet added

--- main.js
+++ main.js
@@ -21,3 +21,20 @@ function openmenu(){
 function closemenu(){
     sidemeu.style.right = "-150px";
 }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxnrdAy8cGkms5rafhERO6bSVONhVZ0qIvRzGjv-tvD0leI4zTE7LtvZoEmWByE0ldv/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => 
+        {msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
